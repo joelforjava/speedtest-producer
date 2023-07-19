fb826ff1dca19f4b60f0118dfdca668476c4a08c
Joel Cochran <joelforjava@outlook.com>
2023-07-19 09:18:41 -0400

    Refactoring config loading

--- speedtest_producer/main.py
+++ speedtest_producer/main.py
@@ -6,7 +6,18 @@ from configparser import ConfigParser
 from pathlib import Path
 
 from confluent_kafka import Producer
+from confluent_kafka.error import ProduceError
 from rich import print
+
+
+POSSIBLE_CONFIG_LOCATIONS = (
+    Path(),
+    Path.home(),
+    Path("/etc/speedtest-producer"),
+    Path("/opt/speedtest-producer"),
+    Path(os.environ.get("SPEEDTEST_PRODUCER_CONF", "..")),
+)
+"""All possible locations to search for the configuration file."""
 
 
 CONFIG_FILE_NAME = "speedtest-producer.ini"
@@ -15,7 +26,7 @@ CONFIG_FILE_NAME = "speedtest-producer.ini"
 
 def load_config():
     config = ConfigParser()
-    for loc in Path(), Path.home(), Path("/etc/speedtest-producer"), Path(os.environ.get("SPEEDTEST_PRODUCER_CONF", "..")):
+    for loc in POSSIBLE_CONFIG_LOCATIONS:
         try: 
             with (loc / CONFIG_FILE_NAME).open("r") as source:
                 config.read_file(source)
@@ -23,7 +34,7 @@ def load_config():
             pass
         else:
             print(f"Found config at {loc}")
-            print(config["KAFKA"]["topic"])
+            # print(config["KAFKA"]["topic"])
 
     return config
 
@@ -40,25 +51,33 @@ BACKUP_FILE_PATH = Path(CONFIG["BACKUP"]["file_path"])
 
 def send_to_kafka(results: str):
     kafka_conf = CONFIG["KAFKA"]
-    # TODO - probably could just pass as-is, but need to separate other items, e.g. topic name and key, out
     conf = {
         "bootstrap.brokers": kafka_conf["bootstrap.brokers"],
         "client.id": kafka_conf["client.id"]
     }
     producer = Producer(conf)
-    producer.produce(kafka_conf["topic"], key=kafka_conf["key"], value=results)
+    try:
+        producer.produce(kafka_conf["topic"], key=kafka_conf["key"], value=results)
+    except ProduceError as e:
+        print(f"Error sending data to Kafka: {e.kafka_message}")
+        write_results(results, to_path=Path(CONFIG["KAFKA"]["file_path"]))
+        # TODO - another script will periodically check this file and retry any entries
+    else:
+        producer.flush()
 
 
-def write_results(results: str):
-    if not BACKUP_FILE_PATH.parent.exists():
-        BACKUP_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
-    print(f"Saving data to: {BACKUP_FILE_PATH}")
-    with BACKUP_FILE_PATH.open("a") as fp:
+def write_results(results: str, to_path=None):
+    if not to_path:
+        to_path = BACKUP_FILE_PATH
+
+    if not to_path.parent.exists():
+        to_path.parent.mkdir(parents=True, exist_ok=True)
+    print(f"Saving data to: {to_path}")
+    with to_path.open("a") as fp:
         fp.write(results)
 
 
 def call_speedtest():
-    # TODO - determine full path!
     cmd_params = [SPEEDTEST_CMD, "--json", "--secure"]
     resp = subprocess.run(cmd_params, capture_output=True, encoding=sys.getdefaultencoding())
 
@@ -73,11 +92,13 @@ def run():
 
     try:
         results = json.loads(resp)
+        results["machine_name"] = CONFIG["KAFKA"]["key"]
     except json.JSONDecodeError:
         print("Unable to parse results")
         results = resp
-
-    print(results)
+    else:
+        print(results)
+        results = json.dumps(results)
 
     if CONFIG["KAFKA"].getboolean("enabled"):
         send_to_kafka(resp)
